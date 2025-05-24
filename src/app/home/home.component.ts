7e8eb6e349ecb6603efcaac08f55481c31c99758
EreGitHub <rtayler561@gmail.com>
2025-05-24 12:51:35 -0400

    Implement session management in HomeComponent: update HTML to conditionally display session status, refactor TypeScript to manage session state and progress, and adjust titles for starting and stopping sessions.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,21 +17,32 @@ import { listen } from '@tauri-apps/api/event';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class HomeComponent {
-  public progress: WritableSignal<string>;
+  public isSessionRunning: WritableSignal<boolean>;
+  public progress: WritableSignal<number>;
+  public title: WritableSignal<string>;
+
+  public readonly START_TITLE = 'Comenzar';
+  public readonly STOP_TITLE = 'Detener';
 
   constructor() {
-    this.progress = signal('0%');
+    this.isSessionRunning = signal(false);
+    this.title = signal(this.START_TITLE);
+    this.progress = signal(0);
   }
 
   public startSession(): void {
     listen<number>('session-progress', event => {
-      this.progress.set(event.payload.toFixed(0) + '%');
+      this.progress.set(Number(event.payload.toFixed(0)));
     });
 
     listen('session-completed', () => {
       console.log('Session completed');
+      this.isSessionRunning.set(false);
+      this.title.set(this.START_TITLE);
     });
 
     invoke('start_session', { totalTimeMs: 5000 });
+    this.isSessionRunning.set(true);
+    this.title.set(this.STOP_TITLE);
   }
 }
